1a8ef57004997fe4e4ae9d4aa98c229ccc70b23c
nga <=>
2023-05-09 22:01:55 +0700

    //update code

--- family-tree/src/main/java/com/example/familytree/controller/MemberController.java
+++ family-tree/src/main/java/com/example/familytree/controller/MemberController.java
@@ -1,6 +1,5 @@
 package com.example.familytree.controller;
 
-import com.example.familytree.FamilyTreeApplication;
 import com.example.familytree.domain.Member;
 import com.example.familytree.exceptions.ExceptionUtils;
 import com.example.familytree.exceptions.FamilyTreeException;
@@ -8,15 +7,16 @@ import com.example.familytree.models.MemberDTO;
 import com.example.familytree.models.common.ErrorDTO;
 import com.example.familytree.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;import org.springframework.web.bind.annotation.PutMapping;import org.springframework.web.bind.annotation.RequestBody;import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @Slf4j
 @RequiredArgsConstructor
 @RestController
